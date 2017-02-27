b65558929db6a1a3e78e3c36d6547ad43b56f827
vtcurry <vtcurry@gmail.com>
2017-02-27 18:39:18 -0500

    first try

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,46 @@
 package sample;
 
-public class Controller {
+import com.sun.tools.internal.xjc.generator.bean.field.ContentListField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    @FXML
+    TextField name;
+    @FXML
+    TextField phone;
+    @FXML
+    TextField email;
+    @FXML
+    ListView list;
+
+    public void add(){
+        if(name.getText().equalsIgnoreCase("") || phone.getText().equalsIgnoreCase("") || email.getText().equalsIgnoreCase("")) {
+
+        } else {
+            contacts.add(new Contact(name.getText(), phone.getText(), email.getText()));
+            name.setText(""); phone.setText(""); email.setText("");
+        }
+    }
+    public void remove(){
+        Contact contact = (Contact) list.getSelectionModel().getSelectedItem();
+
+        contacts.remove(contact);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+        list.setItems(contacts);
+    }
+
+
 }
